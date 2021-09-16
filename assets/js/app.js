443956de230cd9c4f3eadc51d59bc3dedc71267d
rezwan <h.rezwan@outlook.com>
2021-09-16 13:11:18 +0600

    progressbar completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,36 +14,6 @@ let total_currentTime = document.getElementById('current_time');
 
 let progress_div = document.getElementById('progress_div');
 
-// song data
-const songs = [{
-        name: 'jsp-1',
-        track: './assets/music/jsp-1.mp3',
-        cover: './assets/images/jsp-1.jpg',
-        title: 'A night to remember',
-        artist: 'Rezwan',
-    },
-    {
-        name: 'jsp-2',
-        cover: './assets/images/jsp-2.jpg',
-        track: './assets/music/jsp-2.mp3',
-        title: 'Outer Isolation',
-        artist: 'Rezwan',
-    },
-    {
-        name: 'jsp-3',
-        cover: './assets/images/jsp-3.jpeg',
-        track: './assets/music/jsp-3.mp3',
-        title: 'Grace - Ambient Mix',
-        artist: 'Rezwan',
-    },
-    {
-        name: 'jsp-4',
-        cover: './assets/images/jsp-4.jpg',
-        track: './assets/music/jsp-4.mp3',
-        title: 'Pulverized',
-        artist: 'Rezwan',
-    },
-]
 
 
 let isPlaying = false;
---
+++ assets/js/songs.js
@@ -0,0 +1,30 @@
+// song data
+const songs = [{
+        name: 'jsp-1',
+        track: './assets/music/jsp-1.mp3',
+        cover: './assets/images/jsp-1.jpg',
+        title: 'A night to remember',
+        artist: 'Rezwan',
+    },
+    {
+        name: 'jsp-2',
+        cover: './assets/images/jsp-2.jpg',
+        track: './assets/music/jsp-2.mp3',
+        title: 'Outer Isolation',
+        artist: 'Rezwan',
+    },
+    {
+        name: 'jsp-3',
+        cover: './assets/images/jsp-3.jpeg',
+        track: './assets/music/jsp-3.mp3',
+        title: 'Grace - Ambient Mix',
+        artist: 'Rezwan',
+    },
+    {
+        name: 'jsp-4',
+        cover: './assets/images/jsp-4.jpg',
+        track: './assets/music/jsp-4.mp3',
+        title: 'Pulverized',
+        artist: 'Rezwan',
+    },
+]
