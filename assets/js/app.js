b7c8c6fc5566f9bb6b4956d629d883444d866b68
rezwan <h.rezwan@outlook.com>
2021-09-16 12:07:36 +0600

    progressbar completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,12 @@ const artist = document.getElementById('artist');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 
+let progress = document.getElementById('progress');
+let total_duration = document.getElementById('duration');
+let total_currentTime = document.getElementById('current_time');
+
+let progress_div = document.getElementById('progress_div');
+
 // song data
 const songs = [{
         name: 'jsp-1',
@@ -20,8 +26,8 @@ const songs = [{
         name: 'jsp-2',
         cover: './assets/images/jsp-2.jpg',
         track: './assets/music/jsp-2.mp3',
-        title: 'Rezwan',
-        artist: 'Grace - Ambient Mix',
+        title: 'Grace - Ambient Mix',
+        artist: 'Rezwan',
     },
     // {
     //     name: 'jsp-3',
@@ -84,6 +90,62 @@ const prevSong = () => {
     playMusic();
 
 }
+
+// progress bar functionality
+
+audio.addEventListener('timeupdate', (e) => {
+
+    // destructuring elements from timeupdate event
+    const { currentTime, duration } = e.srcElement;
+
+    let progress_time = (currentTime / duration) * 100;
+
+    progress.style.width = `${progress_time}%`;
+
+    // music duration update
+    let min_duration = Math.floor(duration / 60);
+    let sec_duration = Math.floor(duration % 60);
+
+    let update_duration = `${min_duration}: ${sec_duration}`;
+
+    if (duration) {
+        total_duration.textContent = `${update_duration}`;
+    }
+
+    // current time update
+    let min_currentTime = Math.floor(currentTime / 60);
+    let sec_currentTime = Math.floor(currentTime % 60);
+
+
+    if (sec_currentTime < 10) {
+        sec_currentTime = `0${sec_currentTime}`;
+    }
+
+    let update_currentTime = `${min_currentTime}: ${sec_currentTime}`;
+
+    total_currentTime.textContent = `${update_currentTime}`;
+
+
+
+
+
+
+})
+
+// progress on click functinality
+
+progress_div.addEventListener('click', (e) => {
+
+    const { duration } = audio;
+    let move_progress = (e.offsetX / e.srcElement.clientWidth) * duration;
+    audio.currentTime = move_progress;
+    audio.play();
+})
+
+
+// switches to next song when a song is ended
+audio.addEventListener('ended', nextSong);
+
 
 next.addEventListener('click', nextSong);
 prev.addEventListener('click', prevSong);
